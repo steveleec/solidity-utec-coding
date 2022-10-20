07d8d6d8800eed8b04139ee2c263fcc557780b0c
Lee Marreros <lee@dcspark.io>
2022-10-20 01:59:46 -0500

    Set up for project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,6 @@
 const hre = require("hardhat");
 
-async function main() {
-  const MiPrimerToken = await hre.ethers.getContractFactory("MiPrimerToken");
-  const miPrimerToken = await MiPrimerToken.deploy(
-    "Mi Primer Token",
-    "MPRMTKN"
-  );
-
-  var tx = await miPrimerToken.deployed();
-  await tx.deployTransaction.wait(5);
-
-  console.log(`Deploy at ${miPrimerToken.address}`);
-
-  await hre.run("verify:verify", {
-    address: miPrimerToken.address,
-    constructorArguments: ["Mi Primer Token", "MPRMTKN"],
-  });
-}
+async function main() {}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
--- test/testToken.js
+++ test/testToken.js
@@ -1,26 +1,3 @@
 const { expect } = require("chai");
 
-describe("MI PRIMER TOKEN TESTING", function () {
-  var MiPrimerToken, miPrimerToken;
-
-  describe("Set up", () => {
-    it("Deploys correctly", async () => {
-      MiPrimerToken = await hre.ethers.getContractFactory("MiPrimerToken");
-      miPrimerToken = await MiPrimerToken.deploy("Mi Primer Token", "MPRMTKN");
-
-      await miPrimerToken.deployed();
-    });
-  });
-
-  describe("Name y Symbol", () => {
-    it("Retrieves correct token name", async () => {
-      var tokenName = "Mi Primer Token";
-      expect(await miPrimerToken.name()).to.be.equal(tokenName);
-    });
-
-    it("Retrieves correct token symbol", async () => {
-      var tokenSymbol = "MPRMTKN";
-      expect(await miPrimerToken.symbol()).to.be.equal(tokenSymbol);
-    });
-  });
-});
+describe("MI PRIMER TOKEN TESTING", function () {});
