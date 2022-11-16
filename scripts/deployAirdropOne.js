6e56b8fdbb3d80cafa5cb4e77e19143f0d852743
Lee Marreros <lee.marreros@pucp.pe>
2022-11-15 22:01:53 -0500

    Adds sc with abi and without abi

--- scripts/deployAirdropOne.js
+++ scripts/deployAirdropOne.js
@@ -1,4 +1,12 @@
 const hre = require("hardhat");
+const abiCuy = require("./abiCuy");
+require("dotenv").config();
+
+function getRole(role) {
+  return hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes(role));
+}
+var MINTER_ROLE = getRole("MINTER_ROLE");
+var BURNER_ROLE = getRole("BURNER_ROLE");
 
 async function main() {
   /**
@@ -39,12 +47,6 @@ async function main() {
   await airdropOne.setTokenAddress(miPrimerToken.address);
 
   // Set up Roles Token => Airdrop
-  function getRole(role) {
-    return hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes(role));
-  }
-  var MINTER_ROLE = getRole("MINTER_ROLE");
-  var BURNER_ROLE = getRole("BURNER_ROLE");
-
   await miPrimerToken.grantRole(MINTER_ROLE, airdropOne.address);
   await miPrimerToken.grantRole(BURNER_ROLE, airdropOne.address);
   console.log("Set Up finalizado");
@@ -86,7 +88,74 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+
+async function connectWithoutAbi() {
+  var [owner] = await hre.ethers.getSigners();
+
+  // Cuando tienes el SC en codigo (sin ABI)
+  // conectarnos con el contrato MiPrimerToken
+  var addressSC = "0x27C4eC475D579aa992Ea337dA9984cC418BBE285";
+  var MiPrimerToken = await hre.ethers.getContractFactory("MiPrimerToken");
+  var miPrimerToken = MiPrimerToken.attach(addressSC);
+
+  var ts = await miPrimerToken.totalSupply();
+  // Si he consultado un metodo view y su valor de retorno es un uint256
+  // me devuelve en BigNumbers
+  // console.log("Total Supply", ts.toNumber());
+  console.log("Total Supply", ts);
+  console.log("Total Supply formateado", hre.ethers.utils.formatEther(ts));
+  console.log("Total Supply *", ts.mul(ts).toString());
+  console.log("Total Supply +", ts.add(ts).toString());
+  console.log("Total Supply -", ts.sub(ts).toString());
+
+  var miAddress = "0x5387ddeec8ddC004a217d8e172241EB5F900B302";
+  var miBalance = await miPrimerToken.balanceOf(miAddress);
+  console.log("Mi Balance", miBalance.toString());
+
+  // grantRole a owner
+  // var tx = await miPrimerToken
+  //   .connect(owner)
+  //   .grantRole(MINTER_ROLE, owner.address);
+  // await tx.wait(1);
+
+  // acuñar
+  var to = "0x890ECD3d23Ff71c58Fd1E847dCCAf0bC601a3cd3";
+  var amount = hre.ethers.utils.parseEther("1"); // automaticamente agrega 18 ceros
+  var tx = await miPrimerToken.mint(to, amount);
+  await tx.wait(1);
+  var transactionHash = (hash) => `https://mumbai.polygonscan.com/tx/${hash}`;
+  console.log("Transaction hash", transactionHash(tx.hash));
+  console.log("Transaccion se ha procesado 1");
+
+  // var to2 = "0xbC2568Ae7c08501B54D1f53b0A6FB149818feD9E";
+  // var amount = hre.ethers.utils.parseEther("1000"); // automaticamente agrega 18 ceros
+  // var tx = await miPrimerToken.mint(to2, amount);
+  // await tx.wait(1);
+  // console.log("Transaccion se ha procesado 2");
+}
+
+async function connectWithAbi() {
+  var [owner] = await hre.ethers.getSigners();
+
+  // Cuando tienes el el ABI del SC (sin el codigo del SC)
+  var address = "0x04eCd05895CCc3457fe328817B87DCfB62d8EdF3";
+  var abi = abiCuy;
+  var urlProvider = process.env.MUMBAI_TESNET_URL;
+  var provider = new hre.ethers.providers.JsonRpcProvider(urlProvider);
+  // var abiSimplificado = [
+  // "function balanceOf(address owner) external returns(uint256)",
+  // ];
+  var cuyMoche = new hre.ethers.Contract(address, abi, provider);
+  // var cuyMoche = new hre.ethers.Contract(address, abiSimplificado, provider);
+
+  // console.log(cuyMoche);
+  var res = await cuyMoche.connect(owner).balanceOf(address);
+  console.log("balance", res);
+}
+
+// main()
+// connectWithoutAbi();
+connectWithAbi().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
